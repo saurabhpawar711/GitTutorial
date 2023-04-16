e955752d24c5e1b50b531d0444df70776b6df962
saurabhpawar711 <saurabhpawar1107@gmail.com>
2023-04-16 20:54:54 +0530

    querySelector and querySelectorAll

--- dom.js
+++ dom.js
@@ -16,8 +16,18 @@
 // {
 //     itemList[i].style.fontWeight = 'bold'
 // }
-let li = document.getElementsByTagName('li')
-for(let i=0; i<li.length; i++)
+// let li = document.getElementsByTagName('li')
+// for(let i=0; i<li.length; i++)
+// {
+//     li[i].style.fontWeight = 'bold'    
+// }
+// let secondItem = document.querySelector('.list-group-item:nth-child(2)')
+// secondItem.style.color='green'
+// let thirdItem = document.querySelector('.list-group-item:nth-child(3)')
+// thirdItem.style.visibility='hidden'
+
+let odd = document.querySelectorAll('.li:nth-child(odd)')
+for(let i=0; i<odd.length; i++)
 {
-    li[i].style.fontWeight = 'bold'    
+    odd[i].style.backgroundColor = 'green'    
 }
