7457a4ab707da20a4e52abf952237b20b0accb54
saurabhpawar711 <saurabhpawar1107@gmail.com>
2023-04-16 19:53:16 +0530

    new demo

--- dom.js
+++ dom.js
@@ -1,11 +1,18 @@
-console.dir(document);
-console.log(document.title)
-console.log(document.all[10].textContent = 'Hello')
-let title = document.getElementById('header-title')
-let header = document.getElementById('main-header')
-title.textContent = 'World'
-console.log(title.textContent)
-header.style.border="solid 4px #000"
-let titleN = document.getElementById('title-name')
-titleN.style.color = 'green'
-titleN.style.fontWeight = 'bold'
+// console.dir(document);
+// console.log(document.title)
+// console.log(document.all[10].textContent = 'Hello')
+// let title = document.getElementById('header-title')
+// let header = document.getElementById('main-header')
+// title.textContent = 'World'
+// console.log(title.textContent)
+// header.style.border="solid 4px #000"
+// let titleN = document.getElementById('title-name')
+// titleN.style.color = 'green'
+// titleN.style.fontWeight = 'bold'
+let itemList = document.getElementsByClassName('list-group-item')
+console.log(itemList[2].textContent='Hello')
+itemList[2].style.backgroundColor = 'green'
+for(let i=0; i<itemList.length; i++)
+{
+    itemList[i].style.fontWeight = 'bold'
+}
