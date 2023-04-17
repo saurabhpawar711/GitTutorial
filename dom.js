dc897ef68e34f436d11be8be633037eda8df515f
saurabhpawar711 <saurabhpawar1107@gmail.com>
2023-04-17 11:54:51 +0530

    Delete and Edit functionality

--- dom.js
+++ dom.js
@@ -32,8 +32,9 @@
 //     odd[i].style.backgroundColor = 'green'    
 // }
 
-let itemList = document.querySelector('#items')
+// let itemList = document.querySelector('#items')
 // console.log(itemList.parentElement)
+// console.log(itemList.parentNode)
 // itemList.parentElement.style.color='red'
 // itemList.lastElementChild.style.color='red'
 // console.log(itemList.lastChild)
@@ -43,15 +44,50 @@ let itemList = document.querySelector('#items')
 // console.log(itemList.nextElementSibling)
 // console.log(itemList.previousSibling)
 // itemList.previousElementSibling.style.color= 'red'
-let newElement = document.createElement('div')
-newElement.className = 'newE';
-newElement.id = 'newId';
-let text = document.createTextNode('Hello World')
-newElement.appendChild(text)
+// let newElement = document.createElement('div')
+// newElement.className = 'newE';
+// newElement.id = 'newId';
+// newElement.setAttribute('title', 'Hello Ele')
+// let text = document.createTextNode('Hello World')
+// newElement.appendChild(text)
 // console.log(newElement)
 // let addTitle = document.querySelector('header .container')
 // let header = document.querySelector('header h1')
 // addTitle.insertBefore(newElement, header)
-let add = document.querySelector('ul')
-let header = document.querySelector('ul .list-group-item')
-add.insertBefore(newElement, header)
+// let add = document.querySelector('ul')
+// let header = document.querySelector('ul .list-group-item')
+// add.insertBefore(newElement, header)
+
+
+let form = document.getElementById('addForm')
+let itemList = document.getElementById('items')
+
+form.addEventListener('submit',addItem)
+itemList.addEventListener('click', removeItem)
+function addItem(e)
+{
+    e.preventDefault()
+
+    let newItem = document.getElementById('item').value 
+    let text = document.createTextNode(newItem)
+    let list = document.createElement('li')
+    list.className = 'list-group-item'
+    list.appendChild(text)
+    itemList.appendChild(list)
+
+    let delBtn = document.createElement('button')
+    delBtn.className = "btn btn-danger btn-sm float-right delete"
+    delBtn.appendChild(document.createTextNode('X'))
+    list.appendChild(delBtn)
+    
+}
+function removeItem(e)
+{
+    if(e.target.classList.contains('delete'))
+    {
+        let removeList = e.target.parentElement
+        itemList.removeChild(removeList)
+    }
+
+}
+
